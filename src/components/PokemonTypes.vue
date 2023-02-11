c81be4e65c9d6d23a8e5c72a8aaff20f4b47369e
Luis Fritzen <luisfafritzen@gmail.com>
2023-02-11 13:04:40 -0300

    feat: adds new pokemon type tag component

---
+++ src/components/PokemonTypes.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { PokemonType } from "pokenode-ts";
+import { pokemonTypeColors } from "../utils/PokeTypeColors";
+
+const props = defineProps<{
+  pokeTypes: PokemonType | undefined;
+}>();
+
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
