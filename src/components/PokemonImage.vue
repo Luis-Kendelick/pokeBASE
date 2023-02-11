f4c22899e662915477eb9e64d3dc860d885bb212
Luis Fritzen <luisfafritzen@gmail.com>
2023-02-11 13:00:49 -0300

    feat: adds new pokemon image component

---
+++ src/components/PokemonImage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import LoadingSpinner from "@/components/LoadingSpinner.vue";
+import { usePokemonStore } from "@/stores/pokemon";
+import type { PokemonSprites } from "pokenode-ts";
+const props = defineProps<{
+  pokeSprites: PokemonSprites | undefined;
+}>();
+</script>
+<template>
+  <div v-if="props.pokeSprites?.front_default" class="pokemon-photo">
+    <img
+      :src="props.pokeSprites?.front_default"
+      :alt="usePokemonStore().pokemonName"
+    />
+  </div>
+  <LoadingSpinner v-else />
+</template>
+
+<style scoped>
+</style>
