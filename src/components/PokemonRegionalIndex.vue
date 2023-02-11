72d0b0c3eae6d9beae2e2090024946d696be58cc
Luis Fritzen <luisfafritzen@gmail.com>
2023-02-11 13:13:26 -0300

    feat: adds national index component

---
+++ src/components/PokemonRegionalIndex.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { usePokemonStore } from "@/stores/pokemon";
+import type { VersionGameIndex } from "pokenode-ts";
+import { ref, computed } from "vue";
+
+const props = defineProps<{
+  pokemonName: VersionGameIndex[] | undefined;
+}>();
+
+</script>
+
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<style scoped>
+</style>
