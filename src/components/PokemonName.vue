7a92d746027d7b13908fef8a9344542b0e05459f
Luis Fritzen <luisfafritzen@gmail.com>
2023-02-11 12:58:36 -0300

    feat: adds new name component

---
+++ src/components/PokemonName.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { usePokemonStore } from "@/stores/pokemon";
+</script>
+
+<template>
+  <h1>{{ usePokemonStore().pokemonName }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  text-transform: capitalize;
+}
+</style>
