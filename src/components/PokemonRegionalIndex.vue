37462190e2b0e58e5a8d30842fe583db7dfd7979
Luis Fritzen <luisfafritzen@gmail.com>
2023-02-11 13:14:48 -0300

    feat: creates a function to get pokemon national index by game version name

--- src/components/PokemonRegionalIndex.vue
+++ src/components/PokemonRegionalIndex.vue
@@ -7,6 +7,14 @@ const props = defineProps<{
   pokemonName: VersionGameIndex[] | undefined;
 }>();
 
+const selectedVersion = ref("");
+
+const gameIndex = computed(() => {
+  const selected = props.pokemonName?.find(
+    (version) => version.version.name === selectedVersion.value
+  );
+  return selected ? selected.game_index : "";
+});
 </script>
 
 <template>
