c4cfbbaff4ad69718c11531faa4807f3f3b07f81
Luis Fritzen <luisfafritzen@gmail.com>
2023-02-11 13:19:08 -0300

    feat: creates logic to get physical attributes of pokemon

---
+++ src/components/PokemonPhysicalAttrs.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { usePokemonStore } from "@/stores/pokemon";
+
+const physicalAttrs = usePokemonStore();
+
+const computedPhysicalAttrs = computed(() => {
+  return `Altura: ${physicalAttrs.pokemonHeight} | Peso: ${physicalAttrs.pokemonWeight}`;
+});
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
