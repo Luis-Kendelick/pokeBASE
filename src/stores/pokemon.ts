07bfadb587cef1285896ad96c6002d6f3710f7be
Luis Fritzen <luisfafritzen@gmail.com>
2023-02-14 02:33:53 -0300

    fix: fix exported function brackets

--- src/stores/pokemon.ts
+++ src/stores/pokemon.ts
@@ -137,8 +137,10 @@ export const usePokemonColorStore = defineStore("pokemonColor", () => {
         .catch((err) => {
           console.log(err);
         }));
-
+  };
 
   return {
     getPokemonColor,
     pokemonColorValue,
+  };
+});
